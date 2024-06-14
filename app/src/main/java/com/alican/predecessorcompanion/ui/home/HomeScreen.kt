fde171696e46c5b95fa30e2c0ce5e44e430366aa
Aslı <aslibyr00@gmail.com>
2024-06-14 16:02:57 +0300

    Home search function

--- app/src/main/java/com/alican/predecessorcompanion/ui/home/HomeScreen.kt
+++ app/src/main/java/com/alican/predecessorcompanion/ui/home/HomeScreen.kt
@@ -2,9 +2,15 @@ package com.alican.predecessorcompanion.ui.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -18,6 +24,20 @@ fun HomeScreen(
 
 
     Column(Modifier.fillMaxSize()) {
-
+        OutlinedTextField(value = uiState.searchQuery, onValueChange = {
+            viewModel.updateEvent(event = HomeUIStateEvents.UpdateSearchQuery(it))
+        }, keyboardActions = KeyboardActions(
+            onSearch = {
+                viewModel.updateEvent(event = HomeUIStateEvents.onContinue)
+            }
+        ),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search
+            )
+        )
+        uiState.players.forEach {
+            Text(text = it.name)
+        }
     }
 }
--- app/src/main/java/com/alican/predecessorcompanion/ui/home/HomeUIState.kt
+++ app/src/main/java/com/alican/predecessorcompanion/ui/home/HomeUIState.kt
@@ -1,5 +1,7 @@
 package com.alican.predecessorcompanion.ui.home
 
+import com.alican.predecessorcompanion.domain.ui_model.players.PlayersUIModel
+
 sealed class HomeUIStateEvents {
 
     data class UpdateSearchQuery(val searchQuery: String) : HomeUIStateEvents()
@@ -9,5 +11,6 @@ sealed class HomeUIStateEvents {
 
 data class HomeUIStateModel(
     val isLoading: Boolean = false,
-    val searchQuery: String = ""
+    val searchQuery: String = "",
+    val players: List<PlayersUIModel> = emptyList()
 )
