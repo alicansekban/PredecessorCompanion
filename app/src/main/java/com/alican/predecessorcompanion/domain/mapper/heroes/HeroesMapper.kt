87f064b88fe010e33955c08eeb8a75f758c9c241
Aslı <aslibyr00@gmail.com>
2024-06-15 15:24:54 +0300

    Home screen UI

--- app/src/main/java/com/alican/predecessorcompanion/domain/mapper/heroes/HeroesMapper.kt
+++ app/src/main/java/com/alican/predecessorcompanion/domain/mapper/heroes/HeroesMapper.kt
@@ -2,11 +2,14 @@ package com.alican.predecessorcompanion.domain.mapper.heroes
 
 import com.alican.predecessorcompanion.data.remote.response.heroes.HeroesResponse
 import com.alican.predecessorcompanion.domain.ui_model.heroes.HeroesUIModel
+import com.alican.predecessorcompanion.utils.Constant
 
 
 fun HeroesResponse.toUIModel() : HeroesUIModel {
     return HeroesUIModel(
-        heroName = this.name ?:"",
-        heroId = this.id.toString()
+        heroName = this.display_name ?: "",
+        heroId = this.id.toString(),
+        image = (Constant.BASE_URL + this.image),
+        role = this.roles?.get(0) ?: ""
     )
 }
