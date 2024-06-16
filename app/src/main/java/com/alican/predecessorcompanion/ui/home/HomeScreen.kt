20eb65c0fbd94ae560c6fd54625c8ef2c71dd83e
Aslı <aslibyr00@gmail.com>
2024-06-16 14:37:34 +0300

    Refactor HomeScreen composable for more readability

--- app/src/main/java/com/alican/predecessorcompanion/ui/home/HomeScreen.kt
+++ app/src/main/java/com/alican/predecessorcompanion/ui/home/HomeScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalFocusManager
@@ -36,109 +37,118 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.alican.predecessorcompanion.ui.home.components.HeroesScreen
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun HomeScreen(viewModel: HomeViewModel = hiltViewModel()) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-
     val focusRequester = FocusRequester()
     val focusManager = LocalFocusManager.current
-    LaunchedEffect(key1 = uiState.screenType) {
+
+    LaunchedEffect(uiState.screenType) {
         if (uiState.screenType == ScreenType.SEARCH) {
             focusRequester.requestFocus()
         }
-
     }
+
     Column(modifier = Modifier.fillMaxSize()) {
-        AnimatedVisibility(visible = uiState.screenType == ScreenType.HEROES) {
-            Icon(
-                imageVector = Icons.Default.Search,
-                contentDescription = "",
-                modifier = Modifier.clickable {
-                    viewModel.updateEvent(event = HomeUIStateEvents.ChangeScreenType(screenType = ScreenType.SEARCH))
-                })
-        }
-        AnimatedVisibility(visible = uiState.screenType == ScreenType.SEARCH) {
-            Icon(
-                imageVector = Icons.Default.Close,
-                contentDescription = "Close",
-                modifier = Modifier
-                    .clickable {
-                        viewModel.updateEvent(HomeUIStateEvents.ChangeScreenType(ScreenType.HEROES))
-                        focusManager.clearFocus()
-                    }
-                    .padding(12.dp)
-                    .align(Alignment.End)
-            )
-        }
+        SearchIcon(uiState, viewModel, focusManager)
+        HeroesSection(uiState)
+        SearchSection(uiState, focusRequester, viewModel)
+    }
+}
 
-        AnimatedVisibility(visible = uiState.screenType == ScreenType.HEROES) {
-            Column(
-                modifier = Modifier
-                    .padding(16.dp)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                uiState.heroes.groupBy { it.role }.map { (role, heroes) ->
-                    Text(text = role)
-                    FlowRow(
-                        modifier = Modifier.wrapContentSize(),
+@Composable
+fun SearchIcon(uiState: HomeUIStateModel, viewModel: HomeViewModel, focusManager: FocusManager) {
+    AnimatedVisibility(visible = uiState.screenType == ScreenType.HEROES) {
+        Icon(
+            imageVector = Icons.Default.Search,
+            contentDescription = "",
+            modifier = Modifier.clickable {
+                viewModel.updateEvent(HomeUIStateEvents.ChangeScreenType(screenType = ScreenType.SEARCH))
+            }
+        )
+    }
+    AnimatedVisibility(visible = uiState.screenType == ScreenType.SEARCH) {
+        Icon(
+            imageVector = Icons.Default.Close,
+            contentDescription = "Close",
+            modifier = Modifier
+                .clickable {
+                    viewModel.updateEvent(HomeUIStateEvents.ChangeScreenType(ScreenType.HEROES))
+                    focusManager.clearFocus()
+                }
+                .padding(12.dp)
+        )
+    }
+}
 
-                        ) {
-                        heroes.forEach {
-                            HeroesScreen(
-                                hero = it
-                            )
-                        }
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun HeroesSection(uiState: HomeUIStateModel) {
+    AnimatedVisibility(visible = uiState.screenType == ScreenType.HEROES) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            uiState.heroes.groupBy { it.role }.forEach { (role, heroes) ->
+                Text(text = role)
+                FlowRow(modifier = Modifier.wrapContentSize()) {
+                    heroes.forEach { hero ->
+                        HeroesScreen(hero = hero)
                     }
                 }
             }
         }
+    }
+}
 
-        AnimatedVisibility(visible = uiState.screenType == ScreenType.SEARCH) {
-            Column(
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun SearchSection(
+    uiState: HomeUIStateModel,
+    focusRequester: FocusRequester,
+    viewModel: HomeViewModel
+) {
+    AnimatedVisibility(visible = uiState.screenType == ScreenType.SEARCH) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            OutlinedTextField(
+                value = uiState.searchQuery,
+                onValueChange = {
+                    viewModel.updateEvent(HomeUIStateEvents.UpdateSearchQuery(it))
+                },
+                keyboardActions = KeyboardActions(onSearch = {
+                    viewModel.updateEvent(HomeUIStateEvents.OnContinue)
+                }),
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Search
+                ),
                 modifier = Modifier
-                    .padding(16.dp)
-                    .verticalScroll(rememberScrollState())
-            ) {
-                OutlinedTextField(
-                    value = uiState.searchQuery,
-                    onValueChange = {
-                        viewModel.updateEvent(HomeUIStateEvents.UpdateSearchQuery(it))
-                    },
-                    keyboardActions = KeyboardActions(onSearch = {
-                        viewModel.updateEvent(HomeUIStateEvents.OnContinue)
-                    }),
-                    keyboardOptions = KeyboardOptions(
-                        keyboardType = KeyboardType.Text,
-                        imeAction = ImeAction.Search
-                    ),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .focusRequester(focusRequester),
-                    shape = RoundedCornerShape(20.dp),
-                    trailingIcon = {
-                        Icon(
-                            imageVector = Icons.Default.Close,
-                            contentDescription = "Close",
-                            modifier = Modifier
-                                .clickable {
-                                    viewModel.updateEvent(HomeUIStateEvents.UpdateSearchQuery(""))
-                                }
-                                .padding(12.dp)
-                                .align(Alignment.End)
-                        )
-                    },
-                    label = { Text(text = "Search") }
-                )
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
+                shape = RoundedCornerShape(20.dp),
+                trailingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = "Close",
+                        modifier = Modifier
+                            .clickable {
+                                viewModel.updateEvent(HomeUIStateEvents.UpdateSearchQuery(""))
+                            }
+                            .padding(12.dp)
+                            .align(Alignment.End)
+                    )
+                },
+                label = { Text(text = "Search") }
+            )
 
-                FlowRow(
-                    modifier = Modifier.wrapContentSize(),
-
-                    ) {
-                    uiState.players.forEach {
-                        PlayerSearchItem(player = it) {
-                        }
-                    }
+            FlowRow(modifier = Modifier.wrapContentSize()) {
+                uiState.players.forEach { player ->
+                    PlayerSearchItem(player = player) { }
                 }
             }
         }
