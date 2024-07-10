c0417d2aed86d3b0e7f59feb82b67bd89ba9dede
Aslı <aslibyr00@gmail.com>
2024-07-10 20:50:21 +0300

    Added TabRow to SavedScreen

---
+++ app/src/main/java/com/alican/predecessorcompanion/ui/saved/builds/SavedBuildsScreen.kt
@@ -0,0 +1,11 @@
+package com.alican.predecessorcompanion.ui.saved.builds
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SavedBuilsScreen(modifier: Modifier = Modifier) {
+    Text(text = "Saved builds")
+
+}
